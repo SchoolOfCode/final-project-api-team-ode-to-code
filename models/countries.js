42d81dc9d4cee50455d4d2b0bf3ffb807e2bb614
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-02-21 15:21:14 +0000

    added await to countries model

--- models/countries.js
+++ models/countries.js
@@ -1,6 +1,6 @@
 import { query } from "../db/connection.js";
 
 export async function getAllCountries() {
-  const result = query(`SELECT * FROM countries;`);
+  const result = await query(`SELECT * FROM countries;`);
   return result.rows;
 }
