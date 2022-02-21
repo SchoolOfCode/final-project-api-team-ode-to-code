ac71abf8a0ca148aec18bce666c02dc89851e065
bladeyugo88 <pietroannobil@gmail.com>
2022-02-21 15:33:34 +0000

    route check

--- routes/countries.js
+++ routes/countries.js
@@ -1,9 +1,22 @@
 import express from "express";
+import {getAllCountries} from '../models/countries.js';
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "I wish we had some information to give you ☹️" });
-});
 
+/* GET users listing. */
+// router.get("/", function (req, res, next) {
+//   res.json({ message: "I wish we had some information to give you ☹️" });
+// });
+
+
+
+router.get('/', async function (req, res) {
+
+  const allCountries = await getAllCountries();
+  console.log(allCountries)
+   res.json({ 
+    message:'ok',
+    payload:allCountries 
+});
+});
 export default router;
