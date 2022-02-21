d0f334e21aeb1a7aa13ba3dbb545992593f75506
bladeyugo88 <pietroannobil@gmail.com>
2022-02-21 15:18:32 +0000

    route check

--- models/cities.js
+++ models/cities.js
@@ -1,6 +1,6 @@
-import { query } from "../db/connection.js";
+import  query  from "../db/connection.js";
 
 export async function getAllCities() {
-  const result = query(`SELECT * FROM cities;`);
+  const result = await query(`SELECT * FROM cities;`);
   return result.rows;
 }
